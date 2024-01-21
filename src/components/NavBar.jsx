ea9e5e843987cedd8f4cac9cc517f03ec0167ef2
tsegaye27 <ttalegn29@gmail.com>
2024-01-21 09:15:41 +0300

    separating concerns

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <span role="img">🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p className="num-results">
+        Found <strong>{movies.length}</strong> results
+      </p>
+    </nav>
+  );
+}
+
+export default NavBar;
