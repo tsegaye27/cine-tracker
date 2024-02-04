04deafc975f4d12837ad5cb93a769d53c9b68eb1
tsegaye27 <ttalegn29@gmail.com>
2024-02-04 12:33:07 +0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.jsx'
-// import './index.css'
-// import StarRating from "./components/StarRating";
-import TextExpander from "./components/TextExpander";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <StarRating maxRating={5} /> */}
-    <TextExpander>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint doloremque
-      fuga similique dolor error et asperiores consequatur eos vitae minus!
-    </TextExpander>
-    <TextExpander collapsedNumWords={8} color="blue">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint doloremque
-      fuga similique dolor error et asperiores consequatur eos vitae minus!
-    </TextExpander>
-    <TextExpander className="box" expand={true}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint doloremque
-      fuga similique dolor error et asperiores consequatur eos vitae minus!
-    </TextExpander>
+    <App />
   </React.StrictMode>
 );
