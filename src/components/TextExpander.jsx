dde2d6fdec5131675334cb144b5357807d3db217
tsegaye27 <ttalegn29@gmail.com>
2024-02-04 11:37:56 +0300

    text expander challenge

--- src/components/TextExpander.jsx
+++ src/components/TextExpander.jsx
@@ -1,10 +1,35 @@
 import { useState } from "react";
 
-export default function TextExpander({ children }) {
-  const [collapsedNumOfWords, setCollapsedNumOfWords] = useState(0);
-  const [expandButtonText, setExpandButtonText] = useState("Show More");
-  const [collapseButtonText, setCollapseButtonText] = useState("Show Less");
-  const [buttonColor, setButtonColor] = useState("#000");
-
-  return <div></div>;
+export default function TextExpander({
+  children,
+  collapsedNumWords = 5,
+  expandButtonText = "show more",
+  collapseButtonText = "show less",
+  expand = false,
+  className = "",buttonColor
+  color = "red",
+}) {
+  const [isCollapsed, setIsCollapsed] = useState(expand);
+  const btnStyle = {
+    cursor: "pointer",
+    color,
+  };
+  const displayText =
+    children.split(" ").slice(0, collapsedNumWords).join(" ") + `...`;
+  // console.log(words);
+  return (
+    <div>
+      <p>
+        {isCollapsed ? children : displayText}
+        <span>
+          <button style={btnStyle} onClick={() => setIsCollapsed((c) => !c)}>
+            {isCollapsed ? collapseButtonText : expandButtonText}
+          </button>
+        </span>
+      </p>
+      {/* <button onClick={() => setIsCollapsed((c) => !c)}>
+        {isCollapsed ? collapseButtonText : expandButtonText}
+      </button> */}
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
 // import './index.css'
-import StarRating from "./components/StarRating";
+// import StarRating from "./components/StarRating";
 import TextExpander from "./components/TextExpander";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <StarRating maxRating={5} /> */}
-    <TextExpander />
+    <TextExpander>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint doloremque
+      fuga similique dolor error et asperiores consequatur eos vitae minus!
+    </TextExpander>
+    <TextExpander collapsedNumWords={8} color="blue">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint doloremque
+      fuga similique dolor error et asperiores consequatur eos vitae minus!
+    </TextExpander>
+    <TextExpander className="box" expand={true}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint doloremque
+      fuga similique dolor error et asperiores consequatur eos vitae minus!
+    </TextExpander>
   </React.StrictMode>
 );
