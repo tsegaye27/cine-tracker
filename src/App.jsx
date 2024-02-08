9ec7b64fea2621b056bf89f815ecc936bfdf20a7
tsegaye27 <ttalegn29@gmail.com>
2024-02-08 10:47:09 +0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -39,26 +39,33 @@ export default function App() {
       async function fetchMovies() {
         try {
           setIsLoading(true);
+          setError("");
           const res = await fetch(
             `http://www.omdbapi.com/?i=tt3896198&apikey=4e5b907b&s=${query}`
           );
 
           if (!res.ok) {
-            throw new Error("⛔Something went wrong while fetching");
+            throw new Error("Something went wrong while fetching");
           }
 
           const data = await res.json();
           if (data.Response === "False") {
-            throw new Error(`${data.Error}`);
+            throw new Error("Movie Not Found");
           }
           setMovies(data.Search);
         } catch (err) {
-          console.error(err.message);
           setError(err.message);
         } finally {
           setIsLoading(false);
         }
       }
+
+      if (query.length < 2) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
       fetchMovies();
     },
     [query]
@@ -92,7 +99,7 @@ function Loader() {
 function NumResults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>{movies.length}</strong> results
+      Found <strong>{movies ? movies.length : 0}</strong> results
     </p>
   );
 }
