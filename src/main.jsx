ea10838e6b3f7d5a2ad87eec4d351816999438f7
tsegaye27 <ttalegn29@gmail.com>
2024-02-13 00:37:35 +0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+// import App from "./App.jsx";
+import CurrencyConverter from "./components/CurrencyConverter";
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <CurrencyConverter />
   </React.StrictMode>
 );
